bbd999608eedee0713a10f7b2076ed7eff6d69d4
shoeninja1998 <moerani@byui.edu>
2023-09-14 14:20:22 -0600

    Completed Preps 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+        
+        string letter = "A";
+
+        if (percent >= 90){
+            letter = "A";
+        }
+        else if (percent >= 80){
+            letter = "B";
+        }
+        else if (percent >= 70){
+            letter = "C";
+        }
+        else if (percent >=60){
+            letter = "D";
+        }
+        else{
+            letter = "F";
+        }
+    Console.WriteLine($"Your grade is {letter}.");
+
+    if (percent >= 70){
+        Console.WriteLine("You passed!");
+    }
+    else{
+        Console.WriteLine("You did not pass.");
+    }
     }
 }
